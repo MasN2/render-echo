ac010f50534e8710fd1da659c507e1bb48e9ef93
MasN2 <28033394+MasN2@users.noreply.github.com>
2026-05-17 10:31:33 -0400

    Update app.py
    intercept HEAD requests

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ async def health_check(path, request_headers):
     if path == "/healthz":
         return http.HTTPStatus.OK, [], b"OK\n"
 
+    if request.method == "HEAD":
+        # Return a custom response without a body
+        return (200, {"Content-Type": "text/html"}, b"")
+
 
 async def main():
     # Set the stop condition when receiving SIGTERM.
