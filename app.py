ce702a46664e6018d4d842f3e32341ab74208eba
MasN2 <28033394+MasN2@users.noreply.github.com>
2026-05-17 23:10:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,45 @@ import os
 
 from websockets.asyncio.server import serve
 
+waiting_player = None
+
+class Player:
+   def __init__(self, sock):
+      self.socket = sock
+      self.message = None
+      self.opponent = None
+
+async def relay(ws):
+   global waiting_player
+
+   p = Player(ws)
+   if not waiting_player:
+      waiting_player = p
+      while waiting_player is p:
+         await asyncio.sleep(0.01)
+   else:
+      p.opponent = waiting_player
+      waiting_player.opponent = p
+      waiting_player = None
+
+   while True:
+      try:
+         msg = await ws.recv()
+         p.message = msg
+      except websockets.ConnectionClosedError:
+         p.message = "DC"
+      if p.message and p.opponent.message:
+         await p.opponent.socket.send(p.message)
+         await p.socket.send(p.opponent.message)
+         p.message = None
+         p.opponent.message = None
+      while p.message is not None:
+         await asyncio.sleep(0.01)
+
 
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
-
 
 def health_check(connection, request):
     if request.path == "/healthz":
@@ -20,7 +54,7 @@ def health_check(connection, request):
 
 async def main():
     port = int(os.environ["PORT"])
-    async with serve(echo, "", port, process_request=health_check) as server:
+    async with serve(relay, "", port, process_request=health_check) as server:
         loop = asyncio.get_running_loop()
         loop.add_signal_handler(signal.SIGTERM, server.close)
         await server.wait_closed()
